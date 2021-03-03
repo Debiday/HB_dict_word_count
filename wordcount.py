8844f83dc358082db98901c5e215fefd1c0b3341
Deborah <deborah.oyt@gmail.com>
2021-03-03 07:43:07 +0000

    Final commit- working code

---
+++ wordcount.py
@@ -0,0 +1,21 @@
+def make_letter_counts_dict(file):
+    """Create a dictionary that checks how manty times a letter repeats"""
+    dictionary = {}
+
+    with open('twain.txt') as f:
+        for line in f:
+            line = line.strip().split(" ")
+            for words in line:
+                #print(words)
+                if words not in dictionary:
+                    dictionary[words] = 1
+                else:
+                    dictionary[words] += 1
+        return dictionary
+
+def long_form(dictionary):
+    """Changes the dictionary format to print one key value per line"""
+    for i, j in dictionary.items():
+        print(f"{i} {j}")
+
+print(long_form(make_letter_counts_dict('twain.txt')))
